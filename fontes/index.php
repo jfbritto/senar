25aaeba07fafdbdd099ff887361c36031e709710
João Filipi Britto <jf.britto@hotmail.com>
2018-03-05 15:31:21 -0300

    Revert "comitando o sistema"
    This reverts commit 3ac1f0597be1265be2b11b017f051f7fa36a7114.

--- fontes/index.php
+++
@@ -1,1 +0,0 @@
-<?php include('../php/autenticar_sessao.php'); ?>
--- php/autenticar_sessao.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	session_start();
-	if (!isset($_SESSION['senha']) && !isset($_SESSION['senha'])) {
-		header('location: ../index.php?naologado=true');
-		exit;
-	}
-
-?>
